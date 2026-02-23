dbd919e9663379978c2d080330e9ca5c7c982358
Fuaad <fuaadmohamoud@gmail.com>
2026-02-23 11:48:00 -0600

    added name and time

--- PS5/ps5.py
+++ PS5/ps5.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Problem Set 5: Experimental Analysis
-# Name: 
-# Collaborators (discussion):
-# Time:
+# Name: Fuaad Mohamoud
+# Collaborators (discussion): None
+# Time: ~20 hours
 
 import pylab
 import re
